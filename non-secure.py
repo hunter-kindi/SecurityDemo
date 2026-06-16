d4187271b83e908b0b0439db8b996df368c4fe9c
hunter-kindi <94240556+hunter-kindi@users.noreply.github.com>
2026-06-16 10:36:56 -0400

    Update non-secure.py

--- non-secure.py
+++ non-secure.py
@@ -19,6 +19,6 @@ def get_user_status_insecure(username):
     finally:
         conn.close()
 
-# An attacker could use this input to bypass the login or extract data - test 2626
+# An attacker could use this input to bypass the login or extract data - test 2626 TEST
 # Example attack string: "' OR '1'='1"
 # The resulting query would be: "SELECT status FROM users WHERE username = '' OR '1'='1'"
